0401063633f887006c86acd7cc1469a453637da8
sergio <serginhosacf@gmail.com>
2017-03-16 14:10:05 -0500

    metiendo todo el proyecto a git
    primer commit

---
+++ app/src/main/java/com/example/sergio/proyectovehiculos/RegistroGananciaActivity.java
@@ -0,0 +1,13 @@
+package com.example.sergio.proyectovehiculos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroGananciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_ganancia);
+    }
+}
---
+++ app/src/main/java/com/example/sergio/proyectovehiculos/RegistroVehiculoActivity.java
@@ -0,0 +1,13 @@
+package com.example.sergio.proyectovehiculos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroVehiculoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_vehiculo);
+    }
+}
